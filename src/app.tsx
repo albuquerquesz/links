8b1aa75aa3bc01a860cd3b693427a22be93db0bd
Victor Albuquerque <albq.victor@gmail.com>
2025-12-24 00:11:25 -0300

    wip(hero): still changing hero section

--- src/app.tsx
+++ src/app.tsx
@@ -5,25 +5,53 @@ import reactLogo from "./react.svg";
 
 export function App() {
   return (
-    <div id="container" className="w-[1000px] h-screen p-4">
+    <div id="container" className="w-[1000px] h-[800px] p-8 relative bg-dots border">
 
-      {/* HERO */}
-      <section className="w-full">
+      <div className="absolute top-4 left-4 w-12 h-12 border-t-4 border-l-4 border-primary" />
+      <div className="absolute top-4 right-4 w-12 h-12 border-t-4 border-r-4 border-primary" />
+      <div className="absolute bottom-4 left-4 w-12 h-12 border-b-4 border-l-4 border-primary" />
+      <div className="absolute bottom-4 right-4 w-12 h-12 border-b-4 border-r-4 border-primary" />
 
-        <div className="flex gap-4">
-          <aside className="flex-1 border-2 h-[600px]">
-            <h1>VICTOR ALBUQUERQUE</h1>
-          </aside>
+      <div className="w-full h-full border-dashed-white p-8">
 
-          <aside className="flex-1 w-full h-[600px]">
-            <div className="rounded-md w-full h-[300px] p-4 border border-yellow-300">
-              <div className="rounded-xl h-[250px] w-full bg-green-200"></div>
-            </div>
-          </aside>
-        </div>
+        {/* HERO */}
+        <section className="w-full h-full">
+          <div className="flex gap-8 h-full">
+            <aside className="flex-1 flex flex-col justify-start">
+              <div className="flex flex-col space-y-2 mt-10">
+                <span className="text-primary font-mono text-sm mb-4">HUMAN NAME:</span>
+                <h1 className="text-7xl font-black text-primary leading-[0.8] uppercase tracking-tighter">
+                  VICTOR ALBUQUERQUE
+                </h1>
+              </div>
 
-      </section>
+              <div className="mt-20">
+                <p className="text-primary text-xl font-medium max-w-md leading-relaxed">
+                  salve
+                </p>
+              </div>
+            </aside>
 
+            <aside className="flex flex-col border justify-start flex-1 w-full relative">
+              <div className="bg-white shadow-2xl p-4 rotate-2 hover:rotate-0 transition-all duration-500 w-fit mx-auto ">
+                <div className="w-[350px] h-[350px] overflow-hidden bg-gray-100">
+                  <img
+                    src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?q=80&w=2564&auto=format&fit=crop"
+                    alt="Victor Albuquerque"
+                    className="w-full h-full object-cover grayscale hover:grayscale-0 transition-all duration-700"
+                  />
+                </div>
+                <div className="pt-6 pb-2">
+                  <p className="text-primary font-mono text-xl font-bold">VICTOR ALBUQUERQUE</p>
+                  <p className="text-gray-400 font-mono text-xs uppercase tracking-widest">DEVELOPER + DESIGNER 2025</p>
+                </div>
+              </div>
+
+            </aside>
+          </div>
+        </section>
+
+      </div>
     </div>
   );
 }
