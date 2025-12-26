56f45b5c2f6639f7d9be123cb667bd3b13a43c0c
Victor Albuquerque <albq.victor@gmail.com>
2025-12-26 10:30:22 -0300

    dskfskdfsdkfsdf

--- src/app.tsx
+++ src/app.tsx
@@ -16,23 +16,23 @@ const links = [
   {
     type: "article",
     title: "Meu Ambiente de Desenvolvimento",
-    description: "Um tour pela minha trajetória, explicando como cheguei no meu ambiente atual de forma a maximizar minha produtividade.",
+    description: "Um tour pela minha trajetória, mostrando como construí meu ambiente atual com o objetivo de maximizar minha produtividade e bem-estar.",
     image: "https://images.unsplash.com/photo-1629654297299-c8506221ca97?q=80&w=2074&auto=format&fit=crop",
     url: "https://albuquerquesz.medium.com/como-organizar-meu-ambiente-me-torna-um-desenvolvedor-melhor-do-que-você-ecc348b1f94e",
   },
   {
     type: "article",
     title: "Por que usar Go?",
-    description: "Uma análise profunda sobre melhorias de performance e experiência de desenvolvimento comparando Node.js vs Golang para serviços backend.",
-    image: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=2070&auto=format&fit=crop",
-    url: "salve",
+    description: "Uma análise aprofundada sobre o hype crescente em torno do ecossistema Go, comparando-o com outras linguagens e avaliando sua experiência de desenvolvimento.",
+    image: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*mCLuSVdlUaXce2fcvDfILw.png",
+    url: "https://albuquerquesz.medium.com/por-que-usar-go-69361e39800a",
   },
   {
     type: "article",
-    title: "System Design 101",
-    description: "Entendendo os trade-offs em sistemas distribuídos: teorema CAP, padrões de consistência e estratégias de escalabilidade.",
-    image: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=2070&auto=format&fit=crop",
-    url: "salve",
+    title: "A Epidemia do Bun",
+    description: "Bun é só hype ou o futuro do runtime JavaScript? Uma comparação direta com o Node.js.",
+    image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*Qisn0s5XVyC0P4GWfkHy-w.png",
+    url: "",
   },
 ];
 
