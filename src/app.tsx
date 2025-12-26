b676e0889ba82e532cf73f29ec9605e83c1ecaf7
Victor Albuquerque <albq.victor@gmail.com>
2025-12-26 17:27:37 -0300

    dsfsdfsdfsdf

--- src/app.tsx
+++ src/app.tsx
@@ -15,13 +15,6 @@ const links = [
   },
   {
     type: "article",
-    title: "Meu Ambiente de Desenvolvimento",
-    description: "Um tour pela minha trajetória, mostrando como construí meu ambiente atual com o objetivo de maximizar minha produtividade e bem-estar.",
-    image: "https://images.unsplash.com/photo-1629654297299-c8506221ca97?q=80&w=2074&auto=format&fit=crop",
-    url: "https://albuquerquesz.medium.com/como-organizar-meu-ambiente-me-torna-um-desenvolvedor-melhor-do-que-você-ecc348b1f94e",
-  },
-  {
-    type: "article",
     title: "Por que usar Go?",
     description: "Uma análise aprofundada sobre o hype crescente em torno do ecossistema Go, comparando-o com outras linguagens e avaliando sua experiência de desenvolvimento.",
     image: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*mCLuSVdlUaXce2fcvDfILw.png",
@@ -33,6 +26,13 @@ const links = [
     description: "Bun é só hype ou o futuro do runtime JavaScript? Uma comparação direta com o Node.js.",
     image: "https://miro.medium.com/v2/resize:fit:786/format:webp/1*Qisn0s5XVyC0P4GWfkHy-w.png",
     url: "https://albuquerquesz.medium.com/a-epidemia-do-bun-77c76ec17d35",
+  },
+  {
+    type: "article",
+    title: "Meu Ambiente de Desenvolvimento",
+    description: "Um tour pela minha trajetória, mostrando como construí meu ambiente atual com o objetivo de maximizar minha produtividade e bem-estar.",
+    image: "https://images.unsplash.com/photo-1629654297299-c8506221ca97?q=80&w=2074&auto=format&fit=crop",
+    url: "https://albuquerquesz.medium.com/como-organizar-meu-ambiente-me-torna-um-desenvolvedor-melhor-do-que-você-ecc348b1f94e",
   },
 ];
 
