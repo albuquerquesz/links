b21949b345662e71635355e0687e355dcbc5f2c7
Victor Albuquerque <albq.victor@gmail.com>
2026-01-02 08:46:46 -0300

    chore: copyright's year updated

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="w-full py-8 mt-auto border-t border-zinc-300">
       <div className="container mx-auto px-8 flex justify-center items-center text-zinc-500 font-mono uppercase tracking-widest">
-        <p className="text-[10px] md:text-sm text-center">© 2025 VICTOR ALBUQUERQUE ALL RIGHTS RESERVED</p>
+        <p className="text-[10px] md:text-sm text-center">© 2026 VICTOR ALBUQUERQUE ALL RIGHTS RESERVED</p>
       </div>
     </footer>
   )
